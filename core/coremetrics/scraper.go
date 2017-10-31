9240dcdbf52bc034392adcf22c0a9e17e632484f
Lars Gierth <larsg@systemli.org>
2017-10-31 01:34:22 +0100

    WIP
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

---
+++ core/coremetrics/scraper.go
@@ -0,0 +1,12 @@
+package coremetrics
+
+import (
+	// "context"
+	"time"
+	// goprocess "gx/ipfs/QmSF8fPo3jgVBAy8fpdjjYqgG87dkJgUprRBHRd2tmfgpP/goprocess"
+	// gpctx "gx/ipfs/QmSF8fPo3jgVBAy8fpdjjYqgG87dkJgUprRBHRd2tmfgpP/goprocess/context"
+)
+
+type scraper struct {
+	Interval time.Duration
+}
