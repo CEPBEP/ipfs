8ccde5f24bc5bda09b41b656ffaf83952e70b995
Łukasz Magiera <magik6k@gmail.com>
2017-11-25 02:28:30 +0100

    bitswap: remove ProvideBufLen from stat
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

--- core/commands/bitswap.go
+++ core/commands/bitswap.go
@@ -9,7 +9,6 @@ import (
 	e "github.com/ipfs/go-ipfs/core/commands/e"
 	bitswap "github.com/ipfs/go-ipfs/exchange/bitswap"
 	decision "github.com/ipfs/go-ipfs/exchange/bitswap/decision"
-	provider "github.com/ipfs/go-ipfs/providers"
 
 	cid "gx/ipfs/QmNp85zy9RLrQ5oQD4hPyS39ezrrXpcaa7R4Y9kxdWQLLQ/go-cid"
 	"gx/ipfs/QmPSBJL4momYnE7DcUyk2DVhD6rH488ZmHBGLbxNdhU44K/go-humanize"
@@ -165,14 +164,6 @@ var bitswapStatCmd = &cmds.Command{
 			return
 		}
 
-		// ProvideBuf has been moved out of bitswap
-		ps, err := nd.Providers.Stat()
-		if err != nil {
-			res.SetError(err, cmdkit.ErrNormal)
-			return
-		}
-		st.ProvideBufLen = ps.ProvideBufLen
-
 		cmds.EmitOnce(res, st)
 	},
 	Encoders: cmds.EncoderMap{
@@ -183,7 +174,6 @@ var bitswapStatCmd = &cmds.Command{
 			}
 
 			fmt.Fprintln(w, "bitswap status")
-			fmt.Fprintf(w, "\tprovides buffer: %d / %d\n", out.ProvideBufLen, provider.HasBlockBufferSize)
 			fmt.Fprintf(w, "\tblocks received: %d\n", out.BlocksReceived)
 			fmt.Fprintf(w, "\tblocks sent: %d\n", out.BlocksSent)
 			fmt.Fprintf(w, "\tdata received: %d\n", out.DataReceived)
