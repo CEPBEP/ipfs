mlerner/go-ipfs

bitswap: extract provider workers
License: MIT
Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

core/commands/bitswap.go
@@ -8,6 +8,7 @@ import (
 	cmds "github.com/ipfs/go-ipfs/commands"
 	bitswap "github.com/ipfs/go-ipfs/exchange/bitswap"
 	decision "github.com/ipfs/go-ipfs/exchange/bitswap/decision"
+	provider "github.com/ipfs/go-ipfs/exchange/providers"
 
 	cid "gx/ipfs/QmNp85zy9RLrQ5oQD4hPyS39ezrrXpcaa7R4Y9kxdWQLLQ/go-cid"
 	"gx/ipfs/QmPSBJL4momYnE7DcUyk2DVhD6rH488ZmHBGLbxNdhU44K/go-humanize"
@@ -157,6 +158,14 @@ var bitswapStatCmd = &cmds.Command{
 			return
 		}
 
+		// ProvideBuf has been moved out of bitswap
+		ps, err := nd.Providers.Stat()
+		if err != nil {
+			res.SetError(err, cmds.ErrNormal)
+			return
+		}
+		st.ProvideBufLen = ps.ProvideBufLen
+
 		res.SetOutput(st)
 	},
 	Marshalers: cmds.MarshalerMap{
@@ -167,7 +176,7 @@ var bitswapStatCmd = &cmds.Command{
 			}
 			buf := new(bytes.Buffer)
 			fmt.Fprintln(buf, "bitswap status")
-			fmt.Fprintf(buf, "\tprovides buffer: %d / %d\n", out.ProvideBufLen, bitswap.HasBlockBufferSize)
+			fmt.Fprintf(buf, "\tprovides buffer: %d / %d\n", out.ProvideBufLen, provider.HasBlockBufferSize)
 			fmt.Fprintf(buf, "\tblocks received: %d\n", out.BlocksReceived)
 			fmt.Fprintf(buf, "\tblocks sent: %d\n", out.BlocksSent)
 			fmt.Fprintf(buf, "\tdata received: %d\n", out.DataReceived)

exchange/bitswap/testutils.go
@@ -6,13 +6,15 @@ import (
 
 	blockstore "github.com/ipfs/go-ipfs/blocks/blockstore"
 	tn "github.com/ipfs/go-ipfs/exchange/bitswap/testnet"
+	offline "github.com/ipfs/go-ipfs/exchange/offline"
+	providers "github.com/ipfs/go-ipfs/exchange/providers"
 	datastore2 "github.com/ipfs/go-ipfs/thirdparty/datastore2"
 	delay "github.com/ipfs/go-ipfs/thirdparty/delay"
-	testutil "gx/ipfs/QmWRCn8vruNAzHx8i6SAXinuheRitKEGu8c7m26stKvsYx/go-testutil"
 
 	p2ptestutil "gx/ipfs/QmQGX417WoxKxDJeHqouMEmmH4G1RCENNSzkZYHrXy3Xb3/go-libp2p-netutil"
 	ds "gx/ipfs/QmVSase1JP7cq9QkPT46oNwdp9pT6kBkG3oqS14y3QcZjG/go-datastore"
 	ds_sync "gx/ipfs/QmVSase1JP7cq9QkPT46oNwdp9pT6kBkG3oqS14y3QcZjG/go-datastore/sync"
+	testutil "gx/ipfs/QmWRCn8vruNAzHx8i6SAXinuheRitKEGu8c7m26stKvsYx/go-testutil"
 	peer "gx/ipfs/QmXYjuNuxVzXKJCfWasQk1RqkhVLDM9jtUKhqc2WPQmFSB/go-libp2p-peer"
 )
 
@@ -68,6 +70,7 @@ func (g *SessionGenerator) Instances(n int) []Instance {
 type Instance struct {
 	Peer       peer.ID
 	Exchange   *Bitswap
+	Providers  providers.Interface
 	blockstore blockstore.Blockstore
 
 	blockstoreDelay delay.D
@@ -106,6 +109,7 @@ func MkSession(ctx context.Context, net tn.Network, p testutil.Identity) Instanc
 	return Instance{
 		Peer:            p.ID(),
 		Exchange:        bs,
+		Providers:       offline.Providers(), //TODO: make sure this is correct
 		blockstore:      bstore,
 		blockstoreDelay: bsdelay,
 	}

exchange/offline/offline.go
@@ -5,11 +5,12 @@ package offline
 import (
 	"context"
 
-	"github.com/ipfs/go-ipfs/blocks/blockstore"
+	blockstore "github.com/ipfs/go-ipfs/blocks/blockstore"
 	exchange "github.com/ipfs/go-ipfs/exchange"
-	blocks "gx/ipfs/QmSn9Td7xgxm9EV7iEjTckpUWmWApggzPxu7eFGWkkpwin/go-block-format"
+	providers "github.com/ipfs/go-ipfs/exchange/providers"
 
 	cid "gx/ipfs/QmNp85zy9RLrQ5oQD4hPyS39ezrrXpcaa7R4Y9kxdWQLLQ/go-cid"
+	blocks "gx/ipfs/QmSn9Td7xgxm9EV7iEjTckpUWmWApggzPxu7eFGWkkpwin/go-block-format"
 )
 
 func Exchange(bs blockstore.Blockstore) exchange.Interface {
@@ -71,4 +72,20 @@ func (e *offlineExchange) GetBlocks(ctx context.Context, ks []*cid.Cid) (<-chan 
 
 func (e *offlineExchange) IsOnline() bool {
 	return false
+}
+
+type offlineProviders struct{}
+
+func Providers() providers.Interface {
+	return &offlineProviders{}
+}
+
+// Provide always returns nil.
+func (p *offlineProviders) Provide(*cid.Cid) error {
+	return nil
+}
+
+// Provide always returns nil.
+func (p *offlineProviders) Stat() (*providers.Stat, error) {
+	return nil, nil
 }

CEPBEP/ipfs

core/commands/bitswap.go
@@ -9,6 +9,7 @@ import (
 	e "github.com/ipfs/go-ipfs/core/commands/e"
 	bitswap "github.com/ipfs/go-ipfs/exchange/bitswap"
 	decision "github.com/ipfs/go-ipfs/exchange/bitswap/decision"
+	provider "github.com/ipfs/go-ipfs/exchange/providers"
 
 	cmds "gx/ipfs/QmP9vZfc5WSjfGTXmwX2EcicMFzmZ6fXn7HTdKYat6ccmH/go-ipfs-cmds"
 	"gx/ipfs/QmPSBJL4momYnE7DcUyk2DVhD6rH488ZmHBGLbxNdhU44K/go-humanize"
@@ -164,6 +165,14 @@ var bitswapStatCmd = &cmds.Command{
 			return
 		}
 
+		// ProvideBuf has been moved out of bitswap
+		ps, err := nd.Providers.Stat()
+		if err != nil {
+			res.SetError(err, cmdkit.ErrNormal)
+			return
+		}
+		st.ProvideBufLen = ps.ProvideBufLen
+
 		cmds.EmitOnce(res, st)
 	},
 	Encoders: cmds.EncoderMap{
@@ -174,7 +183,7 @@ var bitswapStatCmd = &cmds.Command{
 			}
 
 			fmt.Fprintln(w, "bitswap status")
-			fmt.Fprintf(w, "\tprovides buffer: %d / %d\n", out.ProvideBufLen, bitswap.HasBlockBufferSize)
+			fmt.Fprintf(w, "\tprovides buffer: %d / %d\n", out.ProvideBufLen, provider.HasBlockBufferSize)
 			fmt.Fprintf(w, "\tblocks received: %d\n", out.BlocksReceived)
 			fmt.Fprintf(w, "\tblocks sent: %d\n", out.BlocksSent)
 			fmt.Fprintf(w, "\tdata received: %d\n", out.DataReceived)

exchange/bitswap/testutils.go
@@ -6,14 +6,16 @@ import (
 
 	blockstore "github.com/ipfs/go-ipfs/blocks/blockstore"
 	tn "github.com/ipfs/go-ipfs/exchange/bitswap/testnet"
+	offline "github.com/ipfs/go-ipfs/exchange/offline"
+	providers "github.com/ipfs/go-ipfs/exchange/providers"
 	datastore2 "github.com/ipfs/go-ipfs/thirdparty/datastore2"
 	delay "github.com/ipfs/go-ipfs/thirdparty/delay"
-	testutil "gx/ipfs/QmeDA8gNhvRTsbrjEieay5wezupJDiky8xvCzDABbsGzmp/go-testutil"
 
 	peer "gx/ipfs/QmWNY7dV54ZDYmTA1ykVdwNCqC11mpU4zSUp6XDpLTH9eG/go-libp2p-peer"
 	p2ptestutil "gx/ipfs/QmZTcPxK6VqrwY94JpKZPvEqAZ6tEr1rLrpcqJbbRZbg2V/go-libp2p-netutil"
 	ds "gx/ipfs/QmdHG8MAuARdGHxx4rPQASLcvhz24fzjSQq7AJRAQEorq5/go-datastore"
 	ds_sync "gx/ipfs/QmdHG8MAuARdGHxx4rPQASLcvhz24fzjSQq7AJRAQEorq5/go-datastore/sync"
+	testutil "gx/ipfs/QmeDA8gNhvRTsbrjEieay5wezupJDiky8xvCzDABbsGzmp/go-testutil"
 )
 
 // WARNING: this uses RandTestBogusIdentity DO NOT USE for NON TESTS!
@@ -68,6 +70,7 @@ func (g *SessionGenerator) Instances(n int) []Instance {
 type Instance struct {
 	Peer       peer.ID
 	Exchange   *Bitswap
+	Providers  providers.Interface
 	blockstore blockstore.Blockstore
 
 	blockstoreDelay delay.D
@@ -106,6 +109,7 @@ func MkSession(ctx context.Context, net tn.Network, p testutil.Identity) Instanc
 	return Instance{
 		Peer:            p.ID(),
 		Exchange:        bs,
+		Providers:       offline.Providers(), //TODO: make sure this is correct
 		blockstore:      bstore,
 		blockstoreDelay: bsdelay,
 	}

exchange/offline/offline.go
@@ -5,10 +5,11 @@ package offline
 import (
 	"context"
 
-	"github.com/ipfs/go-ipfs/blocks/blockstore"
+	blockstore "github.com/ipfs/go-ipfs/blocks/blockstore"
 	exchange "github.com/ipfs/go-ipfs/exchange"
-	blocks "gx/ipfs/QmYsEQydGrsxNZfAiskvQ76N2xE9hDQtSAkRSynwMiUK3c/go-block-format"
+	providers "github.com/ipfs/go-ipfs/exchange/providers"
 
+	blocks "gx/ipfs/QmYsEQydGrsxNZfAiskvQ76N2xE9hDQtSAkRSynwMiUK3c/go-block-format"
 	cid "gx/ipfs/QmeSrf6pzut73u6zLQkRFQ3ygt3k6XFT2kjdYP8Tnkwwyg/go-cid"
 )
 
@@ -71,4 +72,20 @@ func (e *offlineExchange) GetBlocks(ctx context.Context, ks []*cid.Cid) (<-chan 
 
 func (e *offlineExchange) IsOnline() bool {
 	return false
+}
+
+type offlineProviders struct{}
+
+func Providers() providers.Interface {
+	return &offlineProviders{}
+}
+
+// Provide always returns nil.
+func (p *offlineProviders) Provide(*cid.Cid) error {
+	return nil
+}
+
+// Provide always returns nil.
+func (p *offlineProviders) Stat() (*providers.Stat, error) {
+	return nil, nil
 }