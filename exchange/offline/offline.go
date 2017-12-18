cybort/go-ipfs

bitswap: extract findProviders to providers
License: MIT
Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

exchange/offline/offline.go
@@ -11,6 +11,7 @@ import (
 
 	cid "gx/ipfs/QmNp85zy9RLrQ5oQD4hPyS39ezrrXpcaa7R4Y9kxdWQLLQ/go-cid"
 	blocks "gx/ipfs/QmSn9Td7xgxm9EV7iEjTckpUWmWApggzPxu7eFGWkkpwin/go-block-format"
+	peer "gx/ipfs/QmXYjuNuxVzXKJCfWasQk1RqkhVLDM9jtUKhqc2WPQmFSB/go-libp2p-peer"
 )
 
 func Exchange(bs blockstore.Blockstore) exchange.Interface {
@@ -89,4 +90,12 @@ func (p *offlineProviders) Provide(*cid.Cid) error {
 // Provide always returns nil.
 func (p *offlineProviders) Stat() (*providers.Stat, error) {
 	return nil, nil
+}
+
+func (p *offlineProviders) FindProviders(ctx context.Context, c *cid.Cid) error {
+	return nil
+}
+
+func (p *offlineProviders) FindProvidersAsync(ctx context.Context, k *cid.Cid, max int) <-chan peer.ID {
+	return nil
 }

providers/providers.go
@@ -8,9 +8,12 @@ import (
 
 	cid "gx/ipfs/QmNp85zy9RLrQ5oQD4hPyS39ezrrXpcaa7R4Y9kxdWQLLQ/go-cid"
 	routing "gx/ipfs/QmPR2JzfKd9poHx9XBhzoFeBBC31ZM3W5iUPKJZWyaoZZm/go-libp2p-routing"
+	pstore "gx/ipfs/QmPgDWmTmuzvP7QE5zwo1TmjbJme9pmZHNujB2453jkCTr/go-libp2p-peerstore"
+	host "gx/ipfs/QmRS46AyqtpJBsf1zmQdeizSDEzo1qkWR7rdEuPFAv8237/go-libp2p-host"
 	process "gx/ipfs/QmSF8fPo3jgVBAy8fpdjjYqgG87dkJgUprRBHRd2tmfgpP/goprocess"
 	procctx "gx/ipfs/QmSF8fPo3jgVBAy8fpdjjYqgG87dkJgUprRBHRd2tmfgpP/goprocess/context"
 	logging "gx/ipfs/QmSpJByNKFX1sCsHBEp3R73FL4NF6FnQTEGyNAXHm2GS52/go-log"
+	peer "gx/ipfs/QmXYjuNuxVzXKJCfWasQk1RqkhVLDM9jtUKhqc2WPQmFSB/go-libp2p-peer"
 )
 
 var (
@@ -21,19 +24,37 @@ var (
 
 	provideWorkerMax = 512
 	provideTimeout   = time.Second * 15
+
+	// maxProvidersPerRequest specifies the maximum number of providers desired
+	// from the network. This value is specified because the network streams
+	// results.
+	// TODO: if a 'non-nice' strategy is implemented, consider increasing this value
+	maxProvidersPerRequest = 3
+	providerRequestTimeout = time.Second * 10
+
+	sizeBatchRequestChan = 32
 )
 
 var log = logging.Logger("providers")
 
+type blockRequest struct {
+	Cid *cid.Cid
+	Ctx context.Context
+}
+
 // Interface is an definition of providers interface to libp2p routing system
 type Interface interface {
 	Provide(*cid.Cid) error
+	FindProviders(ctx context.Context, c *cid.Cid) error
+	FindProvidersAsync(ctx context.Context, k *cid.Cid, max int) <-chan peer.ID
+
 	Stat() (*Stat, error)
 }
 
 type providers struct {
 	routing routing.ContentRouting
 	process process.Process
+	host    host.Host
 
 	// newBlocks is a channel for newly added blocks to be provided to the
 	// network.  blocks pushed down this channel get buffered and fed to the
@@ -41,6 +62,9 @@ type providers struct {
 	newBlocks chan *cid.Cid
 	// provideKeys directly feeds provide workers
 	provideKeys chan *cid.Cid
+
+	// findKeys sends keys to a worker to find and connect to providers for them
+	findKeys chan *blockRequest
 }
 
 func init() {
@@ -53,7 +77,7 @@ func init() {
 
 // NewProviders returns providers interface implementation based on
 // libp2p routing
-func NewProviders(parent context.Context, routing routing.ContentRouting) Interface {
+func NewProviders(parent context.Context, routing routing.ContentRouting, host host.Host) Interface {
 	ctx, cancelFunc := context.WithCancel(parent)
 
 	px := process.WithTeardown(func() error {
@@ -63,9 +87,12 @@ func NewProviders(parent context.Context, routing routing.ContentRouting) Interf
 	p := &providers{
 		routing: routing,
 		process: px,
+		host:    host,
 
 		newBlocks:   make(chan *cid.Cid, HasBlockBufferSize),
 		provideKeys: make(chan *cid.Cid, provideKeysBufferSize),
+
+		findKeys: make(chan *blockRequest, sizeBatchRequestChan),
 	}
 
 	p.startWorkers(ctx, px)
@@ -88,4 +115,49 @@ func (p *providers) Provide(b *cid.Cid) error {
 		return p.process.Close()
 	}
 	return nil
+}
+
+func (p *providers) FindProviders(ctx context.Context, c *cid.Cid) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case p.findKeys <- &blockRequest{Ctx: ctx, Cid: c}:
+		return nil
+	}
+}
+
+// FindProvidersAsync returns a channel of providers for the given key
+func (p *providers) FindProvidersAsync(ctx context.Context, k *cid.Cid, max int) <-chan peer.ID {
+
+	// Since routing queries are expensive, give bitswap the peers to which we
+	// have open connections. Note that this may cause issues if bitswap starts
+	// precisely tracking which peers provide certain keys. This optimization
+	// would be misleading. In the long run, this may not be the most
+	// appropriate place for this optimization, but it won't cause any harm in
+	// the short term.
+	connectedPeers := p.host.Network().Peers()
+	out := make(chan peer.ID, len(connectedPeers)) // just enough buffer for these connectedPeers
+	for _, id := range connectedPeers {
+		if id == p.host.ID() {
+			continue // ignore self as provider
+		}
+		out <- id
+	}
+
+	go func() {
+		defer close(out)
+		providers := p.routing.FindProvidersAsync(ctx, k, max)
+		for info := range providers {
+			if info.ID == p.host.ID() {
+				continue // ignore self as provider
+			}
+			p.host.Peerstore().AddAddrs(info.ID, info.Addrs, pstore.TempAddrTTL)
+			select {
+			case <-ctx.Done():
+				return
+			case out <- info.ID:
+			}
+		}
+	}()
+	return out
 }

providers/workers.go
@@ -2,11 +2,14 @@ package providers
 
 import (
 	"context"
+	"sync"
 
 	cid "gx/ipfs/QmNp85zy9RLrQ5oQD4hPyS39ezrrXpcaa7R4Y9kxdWQLLQ/go-cid"
+	pstore "gx/ipfs/QmPgDWmTmuzvP7QE5zwo1TmjbJme9pmZHNujB2453jkCTr/go-libp2p-peerstore"
 	process "gx/ipfs/QmSF8fPo3jgVBAy8fpdjjYqgG87dkJgUprRBHRd2tmfgpP/goprocess"
 	procctx "gx/ipfs/QmSF8fPo3jgVBAy8fpdjjYqgG87dkJgUprRBHRd2tmfgpP/goprocess/context"
 	logging "gx/ipfs/QmSpJByNKFX1sCsHBEp3R73FL4NF6FnQTEGyNAXHm2GS52/go-log"
+	peer "gx/ipfs/QmXYjuNuxVzXKJCfWasQk1RqkhVLDM9jtUKhqc2WPQmFSB/go-libp2p-peer"
 )
 
 func (p *providers) startWorkers(ctx context.Context, px process.Process) {
@@ -94,6 +97,54 @@ func (p *providers) provideCollector(ctx context.Context) {
 			} else {
 				keysOut = nil
 			}
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (p *providers) providerQueryManager(ctx context.Context) {
+	var activeLk sync.Mutex
+	kset := cid.NewSet()
+
+	for {
+		select {
+		case e := <-p.findKeys:
+			select { // make sure its not already cancelled
+			case <-e.Ctx.Done():
+				continue
+			default:
+			}
+
+			activeLk.Lock()
+			if kset.Has(e.Cid) {
+				activeLk.Unlock()
+				continue
+			}
+			kset.Add(e.Cid)
+			activeLk.Unlock()
+
+			go func(e *blockRequest) {
+				child, cancel := context.WithTimeout(e.Ctx, providerRequestTimeout)
+				defer cancel()
+				providers := p.FindProvidersAsync(child, e.Cid, maxProvidersPerRequest)
+				wg := &sync.WaitGroup{}
+				for pr := range providers {
+					wg.Add(1)
+					go func(pi peer.ID) {
+						defer wg.Done()
+						err := p.host.Connect(ctx, pstore.PeerInfo{ID: pi})
+						if err != nil {
+							log.Debug("failed to connect to provider %s: %s", p, err)
+						}
+					}(pr)
+				}
+				wg.Wait()
+				activeLk.Lock()
+				kset.Remove(e.Cid)
+				activeLk.Unlock()
+			}(e)
+
 		case <-ctx.Done():
 			return
 		}

CEPBEP/ipfs

exchange/offline/offline.go
@@ -9,6 +9,7 @@ import (
 	exchange "github.com/ipfs/go-ipfs/exchange"
 	providers "github.com/ipfs/go-ipfs/providers"
 
+	peer "gx/ipfs/QmWNY7dV54ZDYmTA1ykVdwNCqC11mpU4zSUp6XDpLTH9eG/go-libp2p-peer"
 	blocks "gx/ipfs/QmYsEQydGrsxNZfAiskvQ76N2xE9hDQtSAkRSynwMiUK3c/go-block-format"
 	cid "gx/ipfs/QmeSrf6pzut73u6zLQkRFQ3ygt3k6XFT2kjdYP8Tnkwwyg/go-cid"
 )
@@ -89,4 +90,12 @@ func (p *offlineProviders) Provide(*cid.Cid) error {
 // Provide always returns nil.
 func (p *offlineProviders) Stat() (*providers.Stat, error) {
 	return nil, nil
+}
+
+func (p *offlineProviders) FindProviders(ctx context.Context, c *cid.Cid) error {
+	return nil
+}
+
+func (p *offlineProviders) FindProvidersAsync(ctx context.Context, k *cid.Cid, max int) <-chan peer.ID {
+	return nil
 }

providers/providers.go
@@ -6,10 +6,13 @@ import (
 
 	flags "github.com/ipfs/go-ipfs/flags"
 
+	host "gx/ipfs/QmP46LGWhzVZTMmt5akNNLfoV8qL4h5wTwmzQxLyDafggd/go-libp2p-host"
 	routing "gx/ipfs/QmPCGUjMRuBcPybZFpjhzpifwPP9wPRoiy5geTQKU4vqWA/go-libp2p-routing"
+	pstore "gx/ipfs/QmPgDWmTmuzvP7QE5zwo1TmjbJme9pmZHNujB2453jkCTr/go-libp2p-peerstore"
 	process "gx/ipfs/QmSF8fPo3jgVBAy8fpdjjYqgG87dkJgUprRBHRd2tmfgpP/goprocess"
 	procctx "gx/ipfs/QmSF8fPo3jgVBAy8fpdjjYqgG87dkJgUprRBHRd2tmfgpP/goprocess/context"
 	logging "gx/ipfs/QmSpJByNKFX1sCsHBEp3R73FL4NF6FnQTEGyNAXHm2GS52/go-log"
+	peer "gx/ipfs/QmWNY7dV54ZDYmTA1ykVdwNCqC11mpU4zSUp6XDpLTH9eG/go-libp2p-peer"
 	cid "gx/ipfs/QmeSrf6pzut73u6zLQkRFQ3ygt3k6XFT2kjdYP8Tnkwwyg/go-cid"
 )
 
@@ -21,19 +24,37 @@ var (
 
 	provideWorkerMax = 512
 	provideTimeout   = time.Second * 15
+
+	// maxProvidersPerRequest specifies the maximum number of providers desired
+	// from the network. This value is specified because the network streams
+	// results.
+	// TODO: if a 'non-nice' strategy is implemented, consider increasing this value
+	maxProvidersPerRequest = 3
+	providerRequestTimeout = time.Second * 10
+
+	sizeBatchRequestChan = 32
 )
 
 var log = logging.Logger("providers")
 
+type blockRequest struct {
+	Cid *cid.Cid
+	Ctx context.Context
+}
+
 // Interface is an definition of providers interface to libp2p routing system
 type Interface interface {
 	Provide(*cid.Cid) error
+	FindProviders(ctx context.Context, c *cid.Cid) error
+	FindProvidersAsync(ctx context.Context, k *cid.Cid, max int) <-chan peer.ID
+
 	Stat() (*Stat, error)
 }
 
 type providers struct {
 	routing routing.ContentRouting
 	process process.Process
+	host    host.Host
 
 	// newBlocks is a channel for newly added blocks to be provided to the
 	// network.  blocks pushed down this channel get buffered and fed to the
@@ -41,6 +62,9 @@ type providers struct {
 	newBlocks chan *cid.Cid
 	// provideKeys directly feeds provide workers
 	provideKeys chan *cid.Cid
+
+	// findKeys sends keys to a worker to find and connect to providers for them
+	findKeys chan *blockRequest
 }
 
 func init() {
@@ -53,7 +77,7 @@ func init() {
 
 // NewProviders returns providers interface implementation based on
 // libp2p routing
-func NewProviders(parent context.Context, routing routing.ContentRouting) Interface {
+func NewProviders(parent context.Context, routing routing.ContentRouting, host host.Host) Interface {
 	ctx, cancelFunc := context.WithCancel(parent)
 
 	px := process.WithTeardown(func() error {
@@ -63,9 +87,12 @@ func NewProviders(parent context.Context, routing routing.ContentRouting) Interf
 	p := &providers{
 		routing: routing,
 		process: px,
+		host:    host,
 
 		newBlocks:   make(chan *cid.Cid, HasBlockBufferSize),
 		provideKeys: make(chan *cid.Cid, provideKeysBufferSize),
+
+		findKeys: make(chan *blockRequest, sizeBatchRequestChan),
 	}
 
 	p.startWorkers(ctx, px)
@@ -88,4 +115,49 @@ func (p *providers) Provide(b *cid.Cid) error {
 		return p.process.Close()
 	}
 	return nil
+}
+
+func (p *providers) FindProviders(ctx context.Context, c *cid.Cid) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case p.findKeys <- &blockRequest{Ctx: ctx, Cid: c}:
+		return nil
+	}
+}
+
+// FindProvidersAsync returns a channel of providers for the given key
+func (p *providers) FindProvidersAsync(ctx context.Context, k *cid.Cid, max int) <-chan peer.ID {
+
+	// Since routing queries are expensive, give bitswap the peers to which we
+	// have open connections. Note that this may cause issues if bitswap starts
+	// precisely tracking which peers provide certain keys. This optimization
+	// would be misleading. In the long run, this may not be the most
+	// appropriate place for this optimization, but it won't cause any harm in
+	// the short term.
+	connectedPeers := p.host.Network().Peers()
+	out := make(chan peer.ID, len(connectedPeers)) // just enough buffer for these connectedPeers
+	for _, id := range connectedPeers {
+		if id == p.host.ID() {
+			continue // ignore self as provider
+		}
+		out <- id
+	}
+
+	go func() {
+		defer close(out)
+		providers := p.routing.FindProvidersAsync(ctx, k, max)
+		for info := range providers {
+			if info.ID == p.host.ID() {
+				continue // ignore self as provider
+			}
+			p.host.Peerstore().AddAddrs(info.ID, info.Addrs, pstore.TempAddrTTL)
+			select {
+			case <-ctx.Done():
+				return
+			case out <- info.ID:
+			}
+		}
+	}()
+	return out
 }

providers/workers.go
@@ -2,10 +2,13 @@ package providers
 
 import (
 	"context"
+	"sync"
 
 	process "gx/ipfs/QmSF8fPo3jgVBAy8fpdjjYqgG87dkJgUprRBHRd2tmfgpP/goprocess"
 	procctx "gx/ipfs/QmSF8fPo3jgVBAy8fpdjjYqgG87dkJgUprRBHRd2tmfgpP/goprocess/context"
 	logging "gx/ipfs/QmSpJByNKFX1sCsHBEp3R73FL4NF6FnQTEGyNAXHm2GS52/go-log"
+	peer "gx/ipfs/QmWNY7dV54ZDYmTA1ykVdwNCqC11mpU4zSUp6XDpLTH9eG/go-libp2p-peer"
+	pstore "gx/ipfs/QmYijbtjCxFEjSXaudaQAUz3LN5VKLssm8WCUsRoqzXmQR/go-libp2p-peerstore"
 	cid "gx/ipfs/QmeSrf6pzut73u6zLQkRFQ3ygt3k6XFT2kjdYP8Tnkwwyg/go-cid"
 )
 
@@ -94,6 +97,54 @@ func (p *providers) provideCollector(ctx context.Context) {
 			} else {
 				keysOut = nil
 			}
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (p *providers) providerQueryManager(ctx context.Context) {
+	var activeLk sync.Mutex
+	kset := cid.NewSet()
+
+	for {
+		select {
+		case e := <-p.findKeys:
+			select { // make sure its not already cancelled
+			case <-e.Ctx.Done():
+				continue
+			default:
+			}
+
+			activeLk.Lock()
+			if kset.Has(e.Cid) {
+				activeLk.Unlock()
+				continue
+			}
+			kset.Add(e.Cid)
+			activeLk.Unlock()
+
+			go func(e *blockRequest) {
+				child, cancel := context.WithTimeout(e.Ctx, providerRequestTimeout)
+				defer cancel()
+				providers := p.FindProvidersAsync(child, e.Cid, maxProvidersPerRequest)
+				wg := &sync.WaitGroup{}
+				for pr := range providers {
+					wg.Add(1)
+					go func(pi peer.ID) {
+						defer wg.Done()
+						err := p.host.Connect(ctx, pstore.PeerInfo{ID: pi})
+						if err != nil {
+							log.Debug("failed to connect to provider %s: %s", p, err)
+						}
+					}(pr)
+				}
+				wg.Wait()
+				activeLk.Lock()
+				kset.Remove(e.Cid)
+				activeLk.Unlock()
+			}(e)
+
 		case <-ctx.Done():
 			return
 		}