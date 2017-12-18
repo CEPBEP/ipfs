laurinenas/go-ipfs

providers: Integrate with unixfs/hamt
License: MIT
Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

assets/assets.go
@@ -12,8 +12,8 @@ import (
 	"github.com/ipfs/go-ipfs/core"
 	"github.com/ipfs/go-ipfs/core/coreunix"
 	uio "github.com/ipfs/go-ipfs/unixfs/io"
-	cid "gx/ipfs/QmNp85zy9RLrQ5oQD4hPyS39ezrrXpcaa7R4Y9kxdWQLLQ/go-cid"
 
+	cid "gx/ipfs/QmNp85zy9RLrQ5oQD4hPyS39ezrrXpcaa7R4Y9kxdWQLLQ/go-cid"
 	// this import keeps gx from thinking the dep isn't used
 	_ "gx/ipfs/QmdZ4PvPHFQVLLEve7DgoKDcSY19wwpGBB1GKjjKi2rEL1/dir-index-html"
 )
@@ -45,7 +45,7 @@ func SeedInitDirIndex(nd *core.IpfsNode) (*cid.Cid, error) {
 }
 
 func addAssetList(nd *core.IpfsNode, l []string) (*cid.Cid, error) {
-	dirb := uio.NewDirectory(nd.DAG)
+	dirb := uio.NewDirectory(nd.DAG, nd.Providers)
 
 	for _, p := range l {
 		d, err := Asset(p)

unixfs/io/dirbuilder.go
@@ -6,10 +6,11 @@ import (
 	"os"
 
 	mdag "github.com/ipfs/go-ipfs/merkledag"
+	providers "github.com/ipfs/go-ipfs/providers"
 	format "github.com/ipfs/go-ipfs/unixfs"
 	hamt "github.com/ipfs/go-ipfs/unixfs/hamt"
-	cid "gx/ipfs/QmNp85zy9RLrQ5oQD4hPyS39ezrrXpcaa7R4Y9kxdWQLLQ/go-cid"
 
+	cid "gx/ipfs/QmNp85zy9RLrQ5oQD4hPyS39ezrrXpcaa7R4Y9kxdWQLLQ/go-cid"
 	node "gx/ipfs/QmPN7cwmpcc4DWXb4KTB9dNAJgjuPY69h3npsMfhRrQL9c/go-ipld-format"
 )
 
@@ -26,18 +27,19 @@ var UseHAMTSharding = false
 var DefaultShardWidth = 256
 
 type Directory struct {
-	dserv   mdag.DAGService
-	dirnode *mdag.ProtoNode
+	dserv mdag.DAGService
+	prov  providers.Interface
 
-	shard *hamt.HamtShard
+	dirnode *mdag.ProtoNode
+	shard   *hamt.HamtShard
 }
 
 // NewDirectory returns a Directory. It needs a DAGService to add the Children
-func NewDirectory(dserv mdag.DAGService) *Directory {
+func NewDirectory(dserv mdag.DAGService, prov providers.Interface) *Directory {
 	db := new(Directory)
 	db.dserv = dserv
 	if UseHAMTSharding {
-		s, err := hamt.NewHamtShard(dserv, DefaultShardWidth)
+		s, err := hamt.NewHamtShard(dserv, prov, DefaultShardWidth)
 		if err != nil {
 			panic(err) // will only panic if DefaultShardWidth is a bad value
 		}
@@ -51,7 +53,7 @@ func NewDirectory(dserv mdag.DAGService) *Directory {
 // ErrNotADir implies that the given node was not a unixfs directory
 var ErrNotADir = fmt.Errorf("merkledag node was not a directory or shard")
 
-func NewDirectoryFromNode(dserv mdag.DAGService, nd node.Node) (*Directory, error) {
+func NewDirectoryFromNode(dserv mdag.DAGService, prov providers.Interface, nd node.Node) (*Directory, error) {
 	pbnd, ok := nd.(*mdag.ProtoNode)
 	if !ok {
 		return nil, ErrNotADir
@@ -69,7 +71,7 @@ func NewDirectoryFromNode(dserv mdag.DAGService, nd node.Node) (*Directory, erro
 			dirnode: pbnd.Copy().(*mdag.ProtoNode),
 		}, nil
 	case format.THAMTShard:
-		shard, err := hamt.NewHamtFromDag(dserv, nd)
+		shard, err := hamt.NewHamtFromDag(dserv, prov, nd)
 		if err != nil {
 			return nil, err
 		}
@@ -111,7 +113,7 @@ func (d *Directory) AddChild(ctx context.Context, name string, nd node.Node) err
 }
 
 func (d *Directory) switchToSharding(ctx context.Context) error {
-	s, err := hamt.NewHamtShard(d.dserv, DefaultShardWidth)
+	s, err := hamt.NewHamtShard(d.dserv, d.prov, DefaultShardWidth)
 	if err != nil {
 		return err
 	}

CEPBEP/ipfs

assets/assets.go
@@ -12,8 +12,8 @@ import (
 	"github.com/ipfs/go-ipfs/core"
 	"github.com/ipfs/go-ipfs/core/coreunix"
 	uio "github.com/ipfs/go-ipfs/unixfs/io"
-	cid "gx/ipfs/QmeSrf6pzut73u6zLQkRFQ3ygt3k6XFT2kjdYP8Tnkwwyg/go-cid"
 
+	cid "gx/ipfs/QmeSrf6pzut73u6zLQkRFQ3ygt3k6XFT2kjdYP8Tnkwwyg/go-cid"
 	// this import keeps gx from thinking the dep isn't used
 	_ "gx/ipfs/QmdZ4PvPHFQVLLEve7DgoKDcSY19wwpGBB1GKjjKi2rEL1/dir-index-html"
 )
@@ -45,7 +45,7 @@ func SeedInitDirIndex(nd *core.IpfsNode) (*cid.Cid, error) {
 }
 
 func addAssetList(nd *core.IpfsNode, l []string) (*cid.Cid, error) {
-	dirb := uio.NewDirectory(nd.DAG)
+	dirb := uio.NewDirectory(nd.DAG, nd.Providers)
 
 	for _, p := range l {
 		d, err := Asset(p)

unixfs/io/dirbuilder.go
@@ -6,11 +6,12 @@ import (
 	"os"
 
 	mdag "github.com/ipfs/go-ipfs/merkledag"
+	providers "github.com/ipfs/go-ipfs/providers"
 	format "github.com/ipfs/go-ipfs/unixfs"
 	hamt "github.com/ipfs/go-ipfs/unixfs/hamt"
-	cid "gx/ipfs/QmeSrf6pzut73u6zLQkRFQ3ygt3k6XFT2kjdYP8Tnkwwyg/go-cid"
 
 	node "gx/ipfs/QmNwUEK7QbwSqyKBu3mMtToo8SUc6wQJ7gdZq4gGGJqfnf/go-ipld-format"
+	cid "gx/ipfs/QmeSrf6pzut73u6zLQkRFQ3ygt3k6XFT2kjdYP8Tnkwwyg/go-cid"
 )
 
 // ShardSplitThreshold specifies how large of an unsharded directory
@@ -26,18 +27,19 @@ var UseHAMTSharding = false
 var DefaultShardWidth = 256
 
 type Directory struct {
-	dserv   mdag.DAGService
-	dirnode *mdag.ProtoNode
+	dserv mdag.DAGService
+	prov  providers.Interface
 
-	shard *hamt.HamtShard
+	dirnode *mdag.ProtoNode
+	shard   *hamt.HamtShard
 }
 
 // NewDirectory returns a Directory. It needs a DAGService to add the Children
-func NewDirectory(dserv mdag.DAGService) *Directory {
+func NewDirectory(dserv mdag.DAGService, prov providers.Interface) *Directory {
 	db := new(Directory)
 	db.dserv = dserv
 	if UseHAMTSharding {
-		s, err := hamt.NewHamtShard(dserv, DefaultShardWidth)
+		s, err := hamt.NewHamtShard(dserv, prov, DefaultShardWidth)
 		if err != nil {
 			panic(err) // will only panic if DefaultShardWidth is a bad value
 		}
@@ -51,7 +53,7 @@ func NewDirectory(dserv mdag.DAGService) *Directory {
 // ErrNotADir implies that the given node was not a unixfs directory
 var ErrNotADir = fmt.Errorf("merkledag node was not a directory or shard")
 
-func NewDirectoryFromNode(dserv mdag.DAGService, nd node.Node) (*Directory, error) {
+func NewDirectoryFromNode(dserv mdag.DAGService, prov providers.Interface, nd node.Node) (*Directory, error) {
 	pbnd, ok := nd.(*mdag.ProtoNode)
 	if !ok {
 		return nil, ErrNotADir
@@ -69,7 +71,7 @@ func NewDirectoryFromNode(dserv mdag.DAGService, nd node.Node) (*Directory, erro
 			dirnode: pbnd.Copy().(*mdag.ProtoNode),
 		}, nil
 	case format.THAMTShard:
-		shard, err := hamt.NewHamtFromDag(dserv, nd)
+		shard, err := hamt.NewHamtFromDag(dserv, prov, nd)
 		if err != nil {
 			return nil, err
 		}
@@ -111,7 +113,7 @@ func (d *Directory) AddChild(ctx context.Context, name string, nd node.Node) err
 }
 
 func (d *Directory) switchToSharding(ctx context.Context) error {
-	s, err := hamt.NewHamtShard(d.dserv, DefaultShardWidth)
+	s, err := hamt.NewHamtShard(d.dserv, d.prov, DefaultShardWidth)
 	if err != nil {
 		return err
 	}